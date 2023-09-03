8534f01fd3c01c2f28af129c16fd467ea6494e5e
ItsRichard7 <ricardobm@estudiantec.cr>
2023-09-03 14:29:33 -0600

    Add store nodes in list variable functionality

--- Frontend.rkt
+++ Frontend.rkt
@@ -4,7 +4,7 @@
 (define frame (new frame%
                    [label "Simple GUI"]
                    [width 1200]
-                   [height 700]
+                   [height 690]
                    [stretchable-width #f]
                    [stretchable-height #f]))
 
@@ -14,35 +14,66 @@
 ; Make the two left panels show in vertical form
 (define verticalBox (new vertical-panel% [parent horizontalBox]
                                          [min-width 300]))
-
+;------------------------------------------------------------------------------------------------------------------
 ; Panel that contains insert nodes funtionality
 (define InsNodesPanel (new vertical-panel% [parent verticalBox]
-                                  [border 100]
-                                  [min-height 350]))
-
-; Panel that contains insert relations between nodes functionality
-(define InsRelPanel (new panel% [parent verticalBox]
-                                [border 100]
-                                [min-height 350]))
-
-; Panel that contains graph graphic representation
-(define drawGraphPanel (new panel% [parent horizontalBox]
-                                   [min-width 900]))
+                                  [border 50]
+                                  [min-height 230]))   
 
 ; Add a text field to InsNodes Panel that get names of the Nodes from user
 (define text-field (new text-field% [parent InsNodesPanel]
                                     [label "Insert your nodes here: "]
-                                    [font (make-font #:size 15 #:weight 'bold)]))
+                                    [font (make-font #:size 16 #:weight 'bold)]
+                                    [min-width 280]
+                                    [vert-margin 1]))
  
 ; Add a horizontal panel to the dialog, with centering for buttons
 (define panel (new horizontal-panel% [parent InsNodesPanel]
                                      [alignment '(center center)]))
  
 ; Add Cancel and Ok buttons to the horizontal panel
-(new button% [parent panel] [label "Cancel"])
-(new button% [parent panel] [label "Ok"])
-(when (system-position-ok-before-cancel?)
-  (send panel change-children reverse))
+(define cancelBut (new button% [parent panel] 
+                               [label "Cancel"]
+                               [font (make-font #:size 15 #:weight 'bold)]
+                               [min-width 100]
+                               [min-height 50]
+                               [callback (lambda (button event)
+                                  (send text-field set-value "") )]))
+
+(define insertBut (new button% [parent panel] 
+                               [label "Insert"]
+                               [font (make-font #:size 15 #:weight 'bold)]
+                               [min-width 100]
+                               [min-height 50]
+                               [callback (lambda (button event)
+                                  (define text (send text-field get-value))
+                                  (when (not (string=? text ""))
+                                    (set! inserted-nodes (cons text inserted-nodes))
+                                    (printf "Inserted: ~a\n" text)
+                                    (send text-field set-value ""))) ; Clear the text field after insert
+                                  ]))  
+
+(define inserted-nodes '()) ; Initialize an empty list to store inserted nodes
+
+;-------------------------------------------------------------------------------------------------------------------                                  
+
+; Panel that contains insert relations between nodes functionality
+(define InsRelPanel (new panel% [parent verticalBox]
+                                [border 50]
+                                [min-height 230]))
+
+;------------------------------------------------------------------------------------------------------------------                                
+
+; Panel that contains insert relations between nodes functionality
+(define getDirPanel (new panel% [parent verticalBox]
+                                [border 50]
+                                [min-height 230]))
+
+;------------------------------------------------------------------------------------------------------------------                                
+
+; Panel that contains graph graphic representation
+(define drawGraphPanel (new panel% [parent horizontalBox]
+                                   [min-width 900]))
 
 (define world-message (new message% [parent InsRelPanel]
                         [label "World"]))
@@ -57,12 +88,5 @@
                        (send dc set-font (make-font #:size 17 #:weight 'bold))
                        (send dc draw-text "Canada" 160 185))])) ; Write a name in middle of the circle X Y
 
+;---------------------------------------------------------------------------------------------------------------
 (send frame show #t)
-
-
-
-
-
-
-
-
